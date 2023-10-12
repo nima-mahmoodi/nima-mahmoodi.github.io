16b6a17379ad26292366492fa6912317e9f80027
sinawic <sina_041_sm@yahoo.com>
2023-10-12 21:41:28 +0330

    added helper

---
+++ helper.tsx
@@ -0,0 +1,141 @@
+
+export const pages = [
+  { title: 'Home', path: '/#hero', active: '#hero' },
+  { title: 'Videos', path: '/#videos', active: '#videos' },
+  { title: 'Music', path: '/#releases', active: '#releases' },
+  { title: 'Contact', path: '/#contact', active: '#contact' }
+]
+
+export const socials = [{
+  title: 'spotify',
+  link: 'https://open.spotify.com/artist/0ki5GSaw6tPqVddyAofYQU?si=hO8zOULLRiCPrUOhEiex3w'
+}, {
+  title: 'youtube',
+  link: 'https://youtube.com/@nimamahmoodi'
+}, {
+  title: 'soundcloud',
+  link: 'https://soundcloud.com/nimamahmoodi'
+}, {
+  title: 'apple',
+  link: 'https://music.apple.com/us/artist/nima-mahmoodi/1645721730'
+}, {
+  title: 'amazon',
+  link: 'https://music.amazon.com/artists/B077XK21Q8/nima-mahmoodi'
+}, {
+  title: 'deezer',
+  link: 'https://deezer.com/en/artist/13685027'
+}, {
+  title: 'tidal',
+  link: 'https://tidal.com/browse/artist/9790314'
+}, {
+  title: 'instagram',
+  link: 'https://instagram.com/inimamahmoodi'
+}, {
+  title: 'facebook',
+  link: 'https://facebook.com/nimamahmoodimusic'
+}, {
+  title: 'twitter',
+  link: 'https://twitter.com/inimamahmoodi'
+}]
+
+export const releases = [
+  {
+    title: "Zaman Doldu ft. Farmina",
+    key: "zaman-doldu",
+    links: [
+      { title: "youtube", link: "https://www.youtube.com/watch?v=eyhdqa1gzYE" },
+      { title: "spotify", link: "https://open.spotify.com/track/4qtfABVX9Lf8W30hdUAuLc?si=8cea7fa41aac4b1c" },
+      { title: "apple", link: "https://music.apple.com/az/album/zaman-doldu-feat-farmina/1706536039?i=1706536040" },
+      { title: "amazon", link: "https://music.amazon.com/albums/B0CHMXVZCZ?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_gmQ1LkeJeoIU3EUQwQRurldfs" },
+      { title: "deezer", link: "https://deezer.page.link/p9B5QrYofienKUZe6" },
+      { title: "soundcloud", link: "https://soundcloud.com/nimamahmoodi/nima-mahmoodi-zaman-doldu-feat-farmina?si=099efdd6e40346e4a5e40c29ef24e74f&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
+      { title: "tidal", link: "https://tidal.com/browse/track/315231100" },
+    ],
+    text: `VERSE 1 &gt;
+          Zaman doldu
+          Giden gitdi
+          Bir ben kaldim
+          Birde nefretim
+          Zor degil
+          Acim keser
+          Neler geçti
+          Buda geçer
+          <hr />
+          CHORUS &gt;
+          Beni sev dedin
+          Sana agladim
+          Koca gonlumu
+          Sana bagladim
+          Sana kirginim
+          Sana vurgunum
+          Ama malesef
+          Sana bagliyim
+          Beni sev dedin
+          Sana agladim
+          Koca gonlumu
+          Sana bagladim
+          Sana kizginim
+          Sana vurgunum
+          Ama malesef
+          Sana bagliyim
+          <hr />
+          VERSE 2 &gt;
+          Bu yalan sevdada
+          Kirilipda vaz gecmedim
+          Cok kirildim hep ama
+          Baska birini sevmedim
+          Kendimden uzak
+          Bir yerde sensiz
+          Unuttum kendimi
+          Ben kimim`
+  }, {
+    title: "Secret ft. Sena",
+    key: "secret",
+    links: [
+      { title: "youtube", link: "https://www.youtube.com/watch?v=Jv5VJMu0dME" },
+      { title: "spotify", link: "https://open.spotify.com/track/6VazasEU3rtBksmzORO8yg?si=e254b61acd3149d0" },
+      { title: "apple", link: "https://music.apple.com/az/album/secret/1691975535?i=1691975956" },
+      { title: "amazon", link: "https://music.amazon.com/albums/B0C736RJK7?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_tyBX3YOwTiszy2LLCQxCkFGkk&trackAsin=B0C7371BH4" },
+      { title: "deezer", link: "https://deezer.page.link/QyoEHsnvmY9BLu8n7" },
+      { title: "soundcloud", link: "https://soundcloud.com/nimamahmoodi/nima-mahmoodi-secret-feat-sena?si=9cb030b5d1e24739bfe787233813e384&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
+      { title: "tidal", link: "https://tidal.com/browse/track/303970395" },
+    ],
+    text: `VERSE 1 &gt;
+          My heart 
+          The Grave of deep secrets
+          İt's dead 
+          Full of evil spirits 
+          <hr />
+          Told me I'm not allowed to talk
+          I was a kid and I just forgot I forgot that 
+          The past is not ever dead 
+          İt'll always follow you until you lose till the end 
+          <hr />
+          CHORUS  &gt;
+          My lies, my tears 
+          I'm drowning in fears 
+          But Nobody can hear my  screams  I am dead 
+          Nightmares I have 
+          Never told anyone else
+          My deepest my darkest dangerous secret 
+          <hr />
+          VERSE 2 &gt;
+          Never dead, past isn't ever dead 
+          There isn't secret to be taken to your silent grave 
+          To yourself, keeping it to yourself
+          And it will Weaken your soul and make you his slave `
+  }, {
+    title: "Ulduz ft. Sena",
+    key: "ulduz",
+    links: [
+      { title: "youtube", link: "https://www.youtube.com/watch?v=i205aQHTrw4" },
+      { title: "spotify", link: "https://open.spotify.com/track/5z8qVK7t3WPMQAJrew5IAd?si=bd8791c151764abb" },
+      { title: "apple", link: "https://music.apple.com/az/album/ulduz-feat-sena/1647844515?i=1647844846" },
+      { title: "amazon", link: "https://music.amazon.com/albums/B0BH19SQ2N?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_PT5lxjYBqUstik7JyvoFNdkru&trackAsin=B0BH1BPF3C" },
+      { title: "deezer", link: "https://deezer.page.link/cAbNaGqoqAm3CXmy8" },
+      { title: "soundcloud", link: "https://soundcloud.com/nimamahmoodi/ulduz-feat-sena?si=7b0a343bf663465e85d863e2538a175c&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
+      { title: "tidal", link: "https://tidal.com/browse/track/251540132" },
+    ],
+    text: `haha ha ha`
+  }
+]
